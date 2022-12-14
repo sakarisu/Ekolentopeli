44a2a121173096b07942ba4afee95e54dd02ca16
Sakari Surenkin <111981437+sakarisu@users.noreply.github.com>
2022-12-14 11:25:02 +0200

    Create database.py

---
+++ PY/database.py
@@ -0,0 +1,19 @@
+import mysql.connector
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Database:
+    def __init__(self):
+        self.conn = mysql.connector.connect(
+            host='127.0.0.1',
+            port=3000,
+            database='ekolento',
+            user='root',
+            password='',
+            autocommit=True
+        )
+
+    def get_conn(self):
+        return self.conn
